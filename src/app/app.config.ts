f2fa589ac5cbb20c64525236eca273c7f5b5815d
Ibrahim Nazzier <ibrahemnazzier@yahoo.com>
2025-12-20 00:14:24 +0200

    Adding HTTPClient And translation to app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,46 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { Observable } from 'rxjs';
+
+// AoT requires an exported function for factories
+export class CustomLoader implements TranslateLoader { 
+  
+  // 2. الحقن (Dependency Injection)
+  constructor(private http: HttpClient) {}
+
+  // 3. المنطق (The Logic)
+  getTranslation(lang: string): Observable<any> {
+    // 4. العملية الفعلية
+    return this.http.get(`./assets/i18n/${lang}.json`);
+  }
+}
+
+export function createTranslateLoader(http: HttpClient) {
+  return new CustomLoader(http);
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    // fetch API provider
+    provideHttpClient(withFetch()),
+
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+        }
+      })
+    )
   ]
 };
